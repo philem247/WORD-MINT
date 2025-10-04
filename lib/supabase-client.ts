be49406df635abc5d231b3cc9d79ed1d48914f28
philem247 <adeoyephilip247@gmail.com>
2025-10-04 06:55:54 +0100

    Updated supabase-client.ts

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
 export type Profile = {
   id: string;
