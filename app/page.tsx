32038d819d38ecad870085fcfe0e8647ad559334
philem247 <adeoyephilip247@gmail.com>
2025-10-04 07:50:49 +0100

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = "force-dynamic";
+
 import GamePanel from "../components/GamePanel";
 import LeaderboardPanel from "../components/LeaderboardPanel";
 import UserStatsPanel from "../components/UserStatsPanel";
