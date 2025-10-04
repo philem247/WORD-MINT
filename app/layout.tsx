51ec7bed56199855cb92c67df96d7a225401e253
philem247 <adeoyephilip247@gmail.com>
2025-10-04 07:55:29 +0100

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
