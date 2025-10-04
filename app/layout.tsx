7f71eb8d250b427c8a5c64e1f03f2cfbd269ba38
philem247 <adeoyephilip247@gmail.com>
2025-10-04 06:47:44 +0100

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers"; // 👈 import your new wrapper
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
